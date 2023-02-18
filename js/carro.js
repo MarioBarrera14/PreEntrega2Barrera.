d106c68aa32b0310e33af67accf29e88c27c7fdf
Javier.Mario <75219747+MarioBarrera14@users.noreply.github.com>
2023-02-18 18:16:30 -0300

    entrega2

--- js/carro.js
+++ js/carro.js
@@ -1,40 +1,45 @@
+let productos = [
+  { nombre: "a13", precio: 73.999 },
+  { nombre: "a53", precio: 163.999 }
+];
+
 let carrito = [];
+let total = 0;
 
 while (true) {
-  let producto = prompt("Ingrese el nombre del producto solo nos queda en stock samsung a53 y a13,(ingrese 'finalizar' para terminar la compra):");
+  let producto = prompt("Ingrese el nombre del producto que desea agregar al carrito (o 'finalizar' para terminar la compra):");
 
-  if (producto === "a53" || producto === "a13") {
-    let precio = prompt(`Ingrese el precio de ${producto}:`);
-    let cantidad = prompt(`Ingrese la cantidad de ${producto} que desea comprar:`);
-    
-    let item = {
-      nombre: producto,
-      precio: parseFloat(precio),
-      cantidad: parseInt(cantidad)
-    };
-
-    carrito.push(item);
-    alert(`${producto} ha sido agregado al carrito.`);
-  } else if (producto === "finalizar") {
+  if (producto === "finalizar") {
     console.log("Productos en el carrito:");
-    console.log(carrito);
+    carrito.forEach(item => {
+      console.log(`${item.nombre} - Precio: $${item.precio} - Cantidad: ${item.cantidad}`);
+    });
 
-    let confirmar = confirm("¿Desea confirmar su compra?");
-    if (confirmar) {
-      let total = 0;
-      for (let i = 0; i < carrito.length; i++) {
-        total += carrito[i].precio * carrito[i].cantidad;
-      }
-
-      alert(`¡Gracias por su compra! El total de su compra es de $${total}.`);
-      break;
+    if (carrito.length === 0) {
+      alert("No hay productos en el carrito.");
     } else {
-      carrito = [];
-      alert("Compra cancelada.");
+      alert(`El total de su compra es de $${total}. ¡Gracias por su compra!`);
     }
-  } else if (producto === null || producto === "") {
     break;
-  } else {
-    alert(`Lo siento, no tenemos ${producto} en stock.`);
   }
+
+  let seleccion = productos.find(item => item.nombre.toLowerCase() === producto.toLowerCase());
+
+  if (!seleccion) {
+    alert("Producto no válido.");
+    continue;
+  }
+
+  let cantidad = prompt(`Ingrese la cantidad de ${producto} que desea comprar:`);
+
+  let item = {
+    nombre: seleccion.nombre,
+    precio: seleccion.precio,
+    cantidad: parseInt(cantidad)
+  };
+
+  carrito.push(item);
+  total += item.precio * item.cantidad;
+
+  alert(`${producto} ha sido agregado al carrito.`);
 }
